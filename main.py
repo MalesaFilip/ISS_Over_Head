020faf5d71e7a5f473760174a3b0d04ef395fa84
Filip Malesa <121855877+MalesaFilip@users.noreply.github.com>
2023-01-17 08:53:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,24 @@ import requests
 from datetime import datetime
 import smtplib
 
-MY_LAT = 52.229675
-MY_LONG = 21.012230
+MY_LAT = "Your latitude as float"
+MY_LONG = "Your longitude as float"
 
-my_email = "malespython@gmail.com"
-password = "pjtxnsjaqxirgjsg"
+my_email = "Your Email as a string"
+password = "Your Email password as string"
 
 
-# If the ISS is close to my current position
-# Tworzymy funkcję, która będzie nam zwracać True, jeżeli pozycja ISS będzie wystarczająco bliska naszej
 def is_close_enough():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
 
-    # Trzeba tu było konwertować na float, bo wyrażenie były stringami
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    # Your position is within +5 or -5 degrees of the ISS position.
     if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LAT + 5:
         return True
 
-
-# Tworzymy funkcję, która będzie zwracać True, jeżeli obecnie jest noc
 def is_night():
     parameters = {
         "lat": MY_LAT,
@@ -39,24 +33,19 @@ def is_night():
     response.raise_for_status()
     data = response.json()
 
-    # Trzeba tu było konwertować na int, bo wyrażenie były stringami
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
     time_now = datetime.now().hour
-
-    # and it is currently dark
+    
     if sunset + 1 < time_now < sunrise - 1:
         return True
 
-
-# Then send me an email to tell me to look up.
 if is_close_enough() and is_night():
-    # BONUS: run the code every 60 seconds.
     time.sleep(60)
     connection = smtplib.SMTP("smtp.gmail.com")
     connection.starttls()
     connection.login(user=my_email, password=password)
     connection.sendmail(from_addr=my_email,
-                        to_addrs="males77@yahoo.com",
+                        to_addrs="Mail to send notification as string",
                         msg="Subject:Look up!\n\nThe ISS is above you!")
